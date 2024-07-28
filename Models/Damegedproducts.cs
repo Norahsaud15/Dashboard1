52095f035b4471d3993490bdbbd307cf74fe6295
Norahsaud15 <nxn16saud@gmail.com>
2024-07-28 23:27:55 +0300

    Add files via upload

---
+++ Models/Damegedproducts.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dashboard1.Models
+{
+    public class Damegedproducts
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public int Qty { get; set; }
+        public int ProductId { get; set; }
+    }
+}
+
+    
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dashboard1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
